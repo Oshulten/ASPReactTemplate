05eff6531810ae2eff0f350d8065e164edbc1192
Oscar Hultén <oshulten@gmail.com>
2024-09-01 19:19:06 +0200

    feat: setup tanstack query, tanstack router and eslint for frontend

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+/* eslint-disable react/react-in-jsx-scope */
+import { createContext, StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { routeTree } from './routeTree.gen'
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { DefaultCatchBoundary } from './components/DefaultCatchBoundary.tsx';
+import { NotFound } from './components/NotFound.tsx';
+import { ApplicationContext, defaultApplicationContext } from './types/AppContext.tsx';
+import './css/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const queryClient = new QueryClient();
+
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+  },
+  defaultPreload: 'intent',
+  defaultPreloadStaleTime: 0,
+  defaultErrorComponent: DefaultCatchBoundary,
+  defaultNotFoundComponent: () => <NotFound />,
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+export const AppContext = createContext<ApplicationContext>(defaultApplicationContext);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <AppContext.Provider value={defaultApplicationContext}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </AppContext.Provider>
+  </StrictMode>,
 )
